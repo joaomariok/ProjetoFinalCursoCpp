91cc77713f1ba7b09a6fa9c091e0ec0a7333f05d
Julia Minetto <juliaminetto@gmail.com>
2024-02-12 15:15:32 -0300

    Customize font color and text on GamingView

--- TrucoGame/TrucoGame/Controls/CTransparentStatic.cpp
+++ TrucoGame/TrucoGame/Controls/CTransparentStatic.cpp
@@ -4,6 +4,15 @@
 
 IMPLEMENT_DYNAMIC(CTransparentStatic, CStatic)
 
+void CTransparentStatic::SetText(LPCTSTR lpszText, COLORREF color, int size, BOOL center)
+{
+    m_strText = lpszText;
+    fontSize = size;
+    foreground = color;
+    alignCenter = center;
+    Invalidate();
+}
+
 BOOL CTransparentStatic::OnEraseBkgnd(CDC* /*pDC*/)
 {
     // Prevent from default background erasing.
@@ -18,7 +27,18 @@ BOOL CTransparentStatic::PreCreateWindow(CREATESTRUCT& cs)
 
 HBRUSH CTransparentStatic::CtlColor(CDC* pDC, UINT /*nCtlColor*/)
 {
+    CFont font;
+    font.CreatePointFont(100, _T("Arial"));
+    LOGFONT lf;
+    font.GetLogFont(&lf);
+    lf.lfWeight = FW_BOLD;
+    font.DeleteObject();
+    font.CreateFontIndirect(&lf);
+    pDC->SelectObject(&font);
+
+    pDC->SetTextColor(foreground);
     pDC->SetBkMode(TRANSPARENT);
+
     return reinterpret_cast<HBRUSH>(GetStockObject(NULL_BRUSH));
 }
 
@@ -30,7 +50,36 @@ void CTransparentStatic::PreSubclassWindow()
     SetWindowLongPtr(m_hWnd, GWL_EXSTYLE, exStyle | WS_EX_TRANSPARENT);
 }
 
+void CTransparentStatic::OnPaint()
+{
+    CPaintDC dc(this);
+
+    CFont font;
+    font.CreatePointFont(fontSize * 10, _T("Arial"));
+
+    LOGFONT lf;
+    font.GetLogFont(&lf);
+    lf.lfWeight = FW_BOLD;
+
+    font.DeleteObject();
+    font.CreateFontIndirect(&lf);
+    dc.SelectObject(&font);
+
+    CRect rcClient;
+    GetClientRect(&rcClient);
+
+    dc.SetTextColor(foreground);
+    dc.SetBkMode(TRANSPARENT);
+
+    CString m_string;
+    GetWindowText(m_string);
+    dc.DrawText(m_strText != "" ? m_strText : m_string, &rcClient, (alignCenter ? DT_CENTER : DT_LEFT) | DT_TOP | DT_VCENTER | DT_SINGLELINE);
+
+    CStatic::OnPaint();
+}
+
 BEGIN_MESSAGE_MAP(CTransparentStatic, CStatic)
     ON_WM_ERASEBKGND()
     ON_WM_CTLCOLOR_REFLECT()
+    ON_WM_PAINT()
 END_MESSAGE_MAP()
--- TrucoGame/TrucoGame/Controls/CTransparentStatic.h
+++ TrucoGame/TrucoGame/Controls/CTransparentStatic.h
@@ -2,11 +2,18 @@
 
 class CTransparentStatic : public CStatic {
 	DECLARE_DYNAMIC(CTransparentStatic)
+public:
+	void SetText(LPCTSTR lpszText, COLORREF color = RGB(0,0,0), int fonSize = 10, BOOL alignCenter = false);
 
 protected:
+	CString m_strText;
+	COLORREF foreground = RGB(0,0,0);
+	int fontSize = 10;
+	BOOL alignCenter = false;
 	afx_msg BOOL OnEraseBkgnd(CDC*);
 	afx_msg BOOL PreCreateWindow(CREATESTRUCT& cs);
 	afx_msg HBRUSH CtlColor(CDC* pDC, UINT);
 	afx_msg void PreSubclassWindow();
+	afx_msg void OnPaint();
 	DECLARE_MESSAGE_MAP()
 };
